03b76bfbb109568e1e1889eb17814178b4b3e4e0
ItaysSofer <sofer.itay@gmail.com>
2017-09-16 15:42:31 +0300

    added naive implementation for junction env

--- env/Junction.py
+++ env/Junction.py
@@ -1,14 +1,91 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+import os
+import sys
+import optparse
+import subprocess
+import numpy as np
+import random
+from sumolib import checkBinary
+import traci
 import gym
+from gym import spaces
+
 
 class Junction(gym.Env):
 
     def __init__(self):
+        self.initTraci()
 
+        self.action_space = spaces.Discrete(4)
+        self.observation_space = spaces.Box(0, 3600, (1, ))
+
+    def _seed(self, seed=None):
+        pass
 
     def _step(self, action):
+        traci.trafficlights.setPhase("0", action)
+        traci.simulationStep()
 
+        observation = np.array(traci.vehicle.getIDCount())
+
+        reward = 5
+
+        done = traci.simulation.getMinExpectedNumber() == 0
+        done = bool(done)
+
+        info = {}
+
+        return observation, reward, done, info
 
     def _reset(self):
-
+        traci.close()
+        self.initTraci()
 
     def _render(self, mode='human', close=False):
+        pass
+
+    def _close(self):
+        traci.close()
+
+    def initTraci(self):
+        sumoBinary = checkBinary('sumo')
+        self.generate_routefile(self)
+        traci.start([sumoBinary, "-c", "data/cross.sumocfg"])
+        traci.trafficlights.setPhase("0", 2)
+
+    def generate_routefile(self):
+        random.seed(42)  # make tests reproducible
+        N = 3600  # number of time steps
+        # demand per second from different directions
+        pWE = 1. / 10
+        pEW = 1. / 11
+        pNS = 1. / 30
+        with open("data/cross.rou.xml", "w") as routes:
+            print("""<routes>
+            <vType id="typeWE" accel="0.8" decel="4.5" sigma="0.5" length="5" minGap="2.5" maxSpeed="16.67" guiShape="passenger"/>
+            <vType id="typeNS" accel="0.8" decel="4.5" sigma="0.5" length="7" minGap="3" maxSpeed="25" guiShape="bus"/>
+
+            <route id="right" edges="51o 1i 2o 52i" />
+            <route id="left" edges="52o 2i 1o 51i" />
+            <route id="down" edges="54o 4i 3o 53i" />""", file=routes)
+            lastVeh = 0
+            vehNr = 0
+            for i in range(N):
+                if random.uniform(0, 1) < pWE:
+                    print('    <vehicle id="right_%i" type="typeWE" route="right" depart="%i" />' % (
+                        vehNr, i), file=routes)
+                    vehNr += 1
+                    lastVeh = i
+                if random.uniform(0, 1) < pEW:
+                    print('    <vehicle id="left_%i" type="typeWE" route="left" depart="%i" />' % (
+                        vehNr, i), file=routes)
+                    vehNr += 1
+                    lastVeh = i
+                if random.uniform(0, 1) < pNS:
+                    print('    <vehicle id="down_%i" type="typeNS" route="down" depart="%i" color="1,0,0"/>' % (
+                        vehNr, i), file=routes)
+                    vehNr += 1
+                    lastVeh = i
+            print("</routes>", file=routes)

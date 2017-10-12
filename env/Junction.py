f55e12b7c67c2e703616d64e5884cfebc235c6ac
ItaysSofer <sofer.itay@gmail.com>
2017-10-12 19:33:02 +0300

    added regular up-down traffic + changed model to the one in dqn atari article + changed Junction env implementation to non naive one

--- env/Junction.py
+++ env/Junction.py
@@ -27,21 +27,35 @@ class Junction(gym.Env):
     }
 
     def __init__(self):
-        self.__firstRun = True
         self.__visualize = False
-        self.action_space = spaces.Discrete(4)
-        self.observation_space = spaces.Box(np.array([0]), np.array([3600]))
+        self.__firstRun = True
+        self.__numOfTLightConfig = 2
+        self.__roadLength = 510
+        self.__discretizationRes = 168
+        self.__observationShape = (self.__discretizationRes, self.__discretizationRes)
+        self.__discretizationStep = self.__roadLength * 2 / self.__discretizationRes
+        centerCoord = (int(self.__discretizationRes / 2), int(self.__discretizationRes / 2))
+        self.__downTLightPos = tuple(np.subtract(centerCoord, (0, 1)))
+        self.__leftTLightPos = centerCoord
+        self.__upTLightPos = tuple(np.subtract(centerCoord, (1, 0)))
+        self.__rightTLightPos = tuple(np.subtract(centerCoord, (1, 1)))
+        self.__maxSpeed = 16.67
+
+        self.action_space = spaces.Discrete(self.__numOfTLightConfig)
+        self.observation_space = spaces.Box(0, 1.0, self.__observationShape)
 
     def _seed(self, seed=None):
         pass
 
     def _step(self, action):
+        self.__tLightSwitched = (traci.trafficlights.getRedYellowGreenState("0") == "GrGr" and action == 1) or (traci.trafficlights.getRedYellowGreenState("0") == "rGrG" and action == 0)
+
         traci.trafficlights.setPhase("0", action)
         traci.simulationStep()
 
-        observation = np.array([traci.vehicle.getIDCount()])
+        observation = self.__getObservationMatrix()
 
-        reward = -1 * traci.vehicle.getIDCount()
+        reward = self.__getReward()
 
         done = traci.simulation.getMinExpectedNumber() == 0
         done = bool(done)
@@ -57,7 +71,9 @@ class Junction(gym.Env):
             self.__firstRun = False
         self.__initSimulation()
 
-        observation = np.array([traci.vehicle.getIDCount()])
+        self.__switches = 0
+        observation = np.zeros(self.__observationShape)
+
         return observation
 
     def _render(self, mode='human', close=False):
@@ -73,22 +89,23 @@ class Junction(gym.Env):
         else:
             sumoBinary = checkBinary('sumo')
         traci.start([sumoBinary, "-c", os.path.join(os.path.dirname(__file__), "data/cross.sumocfg"), "--start", "--quit-on-end"])
-        traci.trafficlights.setPhase("0", 2)
+        traci.trafficlights.setPhase("0", self.__numOfTLightConfig - 1)
 
     def __generateRoutefile(self):
         random.seed(42)  # make tests reproducible
         N = 3600  # number of time steps
         # demand per second from different directions
         pWE = 1. / 10
-        pEW = 1. / 11
-        pNS = 1. / 30
+        pEW = 1. / 10
+        pNS = 1. / 10
+        pSN = 1. / 10
         with open(os.path.join(os.path.dirname(__file__), "data/cross.rou.xml"), "w") as routes:
             print("""<routes>
-            <vType id="typeWE" accel="0.8" decel="4.5" sigma="0.5" length="5" minGap="2.5" maxSpeed="16.67" guiShape="passenger"/>
-            <vType id="typeNS" accel="0.8" decel="4.5" sigma="0.5" length="7" minGap="3" maxSpeed="25" guiShape="bus"/>
+            <vType id="typeWE" accel="0.8" decel="4.5" sigma="0.5" length="5" minGap="2.5" maxSpeed=\"""" + str(self.__maxSpeed) + """\" guiShape="passenger"/>
 
             <route id="right" edges="51o 1i 2o 52i" />
             <route id="left" edges="52o 2i 1o 51i" />
+            <route id="up" edges="53o 3i 4o 54i" />
             <route id="down" edges="54o 4i 3o 53i" />""", file=routes)
             lastVeh = 0
             vehNr = 0
@@ -104,7 +121,12 @@ class Junction(gym.Env):
                     vehNr += 1
                     lastVeh = i
                 if random.uniform(0, 1) < pNS:
-                    print('    <vehicle id="down_%i" type="typeNS" route="down" depart="%i" color="1,0,0"/>' % (
+                    print('    <vehicle id="down_%i" type="typeWE" route="down" depart="%i" />' % (
+                        vehNr, i), file=routes)
+                    vehNr += 1
+                    lastVeh = i
+                if random.uniform(0, 1) < pSN:
+                    print('    <vehicle id="up_%i" type="typeWE" route="up" depart="%i" />' % (
                         vehNr, i), file=routes)
                     vehNr += 1
                     lastVeh = i
@@ -112,3 +134,50 @@ class Junction(gym.Env):
 
     def setVisualization(self, visualize):
         self.__visualize = visualize
+
+    def __getObservationMatrix(self):
+        observation = np.zeros(self.__observationShape)
+
+        # Add vehicles location
+        vehiclesNames = traci.vehicle.getIDList()
+        for vehicle in vehiclesNames:
+            pos = traci.vehicle.getPosition(vehicle)
+            x = np.math.floor(pos[0] / self.__discretizationStep)
+            y = np.math.floor(pos[1] / self.__discretizationStep)
+            observation[x][y] = 1.
+
+        # Add traffic light state
+        tLightState = traci.trafficlights.getRedYellowGreenState("0")
+        observation[self.__downTLightPos[0], self.__downTLightPos[1]] = 0.333 + int(tLightState[0] == 'G') * 0.333
+        observation[self.__leftTLightPos[0], self.__leftTLightPos[1]] = 0.333 + int(tLightState[1] == 'G') * 0.333
+        observation[self.__upTLightPos[0], self.__upTLightPos[1]] = 0.333 + int(tLightState[2] == 'G') * 0.333
+        observation[self.__rightTLightPos[0], self.__rightTLightPos[1]] = 0.333 + int(tLightState[3] == 'G') * 0.333
+
+        return observation
+
+    def __getReward(self):
+        teleports = self.__getTeleports()
+        if self.__tLightSwitched:
+            self.__switches += 1
+        delay = self._getDelay()
+        waitTime = self.__getWaitTime()
+
+        return -1 * (0.1 * teleports + 0.1 * self.__switches + 0.4 * delay + 0.4 * waitTime)
+
+    def __getTeleports(self):
+        return traci.simulation.getStartingTeleportNumber()
+
+    def __getWaitTime(self):
+        waitTime = 0.
+        vehiclesNames = traci.vehicle.getIDList()
+        for vehicle in vehiclesNames:
+            waitTime += traci.vehicle.getWaitingTime(vehicle)
+        return waitTime
+
+    def _getDelay(self):
+        delay = 0
+        vehiclesNames = traci.vehicle.getIDList()
+        for vehicle in vehiclesNames:
+            delay += (self.__maxSpeed - traci.vehicle.getSpeed(vehicle)) / self.__maxSpeed
+        return delay
+
--- runner.py
+++ runner.py
@@ -2,11 +2,12 @@ import numpy as np
 import keras
 
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten
+from keras.layers import Dense, Activation, Flatten, Permute, Convolution2D
 from keras.optimizers import Adam
+import keras.backend as K
 
 from rl.agents.dqn import DQNAgent
-from rl.policy import BoltzmannQPolicy
+from rl.policy import LinearAnnealedPolicy, EpsGreedyQPolicy
 from rl.memory import SequentialMemory
 
 from env.Junction import Junction
@@ -18,14 +19,51 @@ env = Junction()
 # env.seed(123)
 nb_actions = env.action_space.n
 
-# Next, we build a very simple model.
+
+INPUT_SHAPE = (168, 168)
+WINDOW_LENGTH = 2
+
+# Nature architecture
+# input_shape = (WINDOW_LENGTH,) + INPUT_SHAPE
+# model = Sequential()
+# if K.image_dim_ordering() == 'tf':
+#     # (width, height, channels)
+#     model.add(Permute((2, 3, 1), input_shape=input_shape))
+# elif K.image_dim_ordering() == 'th':
+#     # (channels, width, height)
+#     model.add(Permute((1, 2, 3), input_shape=input_shape))
+# else:
+#     raise RuntimeError('Unknown image_dim_ordering.')
+# model.add(Convolution2D(32, 8, 8, subsample=(4, 4)))
+# model.add(Activation('relu'))
+# model.add(Convolution2D(64, 4, 4, subsample=(2, 2)))
+# model.add(Activation('relu'))
+# model.add(Convolution2D(64, 3, 3, subsample=(1, 1)))
+# model.add(Activation('relu'))
+# model.add(Flatten())
+# model.add(Dense(512))
+# model.add(Activation('relu'))
+# model.add(Dense(nb_actions))
+# model.add(Activation('linear'))
+# print(model.summary())
+
+# NIPS architecture
+input_shape = (WINDOW_LENGTH,) + INPUT_SHAPE
 model = Sequential()
-model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
-model.add(Dense(16))
+if K.image_dim_ordering() == 'tf':
+    # (width, height, channels)
+    model.add(Permute((2, 3, 1), input_shape=input_shape))
+elif K.image_dim_ordering() == 'th':
+    # (channels, width, height)
+    model.add(Permute((1, 2, 3), input_shape=input_shape))
+else:
+    raise RuntimeError('Unknown image_dim_ordering.')
+model.add(Convolution2D(16, 8, 8, subsample=(4, 4)))
 model.add(Activation('relu'))
-model.add(Dense(16))
+model.add(Convolution2D(32, 4, 4, subsample=(2, 2)))
 model.add(Activation('relu'))
-model.add(Dense(16))
+model.add(Flatten())
+model.add(Dense(256))
 model.add(Activation('relu'))
 model.add(Dense(nb_actions))
 model.add(Activation('linear'))
@@ -33,17 +71,20 @@ print(model.summary())
 
 # Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
 # even the metrics!
-memory = SequentialMemory(limit=50000, window_length=1)
-policy = BoltzmannQPolicy()
-dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10,
-               target_model_update=1e-2, policy=policy)
-dqn.compile(Adam(lr=1e-3), metrics=['mae'])
+memory = SequentialMemory(limit=50000, window_length=WINDOW_LENGTH)
+policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05,
+                              nb_steps=50000)
+
+dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memory,
+               nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
+               train_interval=4, delta_clip=1.)
+dqn.compile(Adam(lr=0.00025), metrics=['mae'])
 
 # Okay, now it's time to learn something! We visualize the training here for show, but this
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
 env.setVisualization(False)
-dqn.fit(env, nb_steps=5000, verbose=2)
+dqn.fit(env, nb_steps=600000, verbose=2)
 
 # After training is done, we save the final weights.
 dqn.save_weights('dqn_{}_weights.h5f'.format('TrafficAI'), overwrite=True)
